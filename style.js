d587f84c79b63d39b9974475715239ac386122ca
Nii797 <phanterasonpardus797@gmail.com>
2021-01-22 17:36:26 +0700

    update

---
+++ style.js
@@ -0,0 +1,9 @@
+const umur = 9;
+
+if(umur >= 18){
+    console.log("saya berusia diatas 18 tahun");
+} else if(umur <= 10 ) {
+    console.log("saya berusia dibawah 10 tahun");
+} else {
+    console.log("hahaha");
+}
