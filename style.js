3b135ce722d4e114a84fc193f3fb1ceb98892d7d
Nii797 <phanterasonpardus797@gmail.com>
2021-01-23 11:21:39 +0700

    update style.js

--- style.js
+++ style.js
@@ -1,9 +1,17 @@
-const umur = 9;
+// const umur = 9;
 
-if(umur >= 18){
-    console.log("saya berusia diatas 18 tahun");
-} else if(umur <= 10 ) {
-    console.log("saya berusia dibawah 10 tahun");
-} else {
-    console.log("hahaha");
+// if(umur >= 18){
+//     console.log("saya berusia diatas 18 tahun");
+// } else if(umur <= 10 ) {
+//     console.log("saya berusia dibawah 10 tahun");
+// } else {
+//     console.log("hahaha");
+// }
+
+for(let number = 1; number <= 100; number++){
+    if(number % 5 === 0){
+        console.log("Kelipatan 5")    
+    } else {
+        console.log(number);
+    }
 }
